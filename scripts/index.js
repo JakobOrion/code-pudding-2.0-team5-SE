6415d7182df155e59db9b6581b9e78b02449266b
Jake Bohn <jakoborion@gmail.com>
2023-03-05 00:31:56 -0500

    fix: added age chart and fixed chart height

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,67 @@
+/* Column chart Age Distribution by Gender */
+anychart.onDocumentReady(function () {
+  // Create palette.
+  var customPalette = anychart.palettes.rangeColors();
+	customPalette.items(['#2D0637', '#9E485C']);
+    customPalette.count(2);
+  
+  // setting data
+  var data = [
+    ["18", 10, 0],
+    ["19", 10, 10],
+    ["20", 25, 30],
+    ["21", 108, 183],
+    ["22", 291, 364],
+    ["23", 387, 507],
+    ["24", 409, 454],
+    ["25", 382, 455],
+    ["26", 377, 492],
+    ["27", 618, 441],
+    ["28", 430, 316],
+    ["29", 362, 227],
+    ["30", 363, 211],
+    ["31", 71, 54],
+    ["32", 153, 57],
+    ["33", 36, 125],
+    ["34", 69, 83],
+    ["35", 0, 60],
+    ["36", 20, 25],
+    ["37", 5, 0],
+    ["38", 0, 19],
+    ["39", 18, 0],
+    ["42", 20, 0],
+    ["55", 0, 6],
+  ];
+
+  var chart = anychart.column();
+  chart.palette(customPalette);
+  
+  chart.title("Age Distribution by Gender");
+  chart.legend(true);
+  chart.title().fontFamily('Outfit');
+  chart.title().fontColor('hsla(288, 59%, 22%, 1)');
+  chart.title().fontSize(20);
+  chart.background().fill("hsla(0, 88%, 90%, 1)");
+  chart.labels().fontFamily('Outfit');
+  chart.labels().fontSize(40);
+  var dataSet = anychart.data.set(data);
+  var mapping1 = dataSet.mapAs({x: 0, value: 1});
+  var mapping2 = dataSet.mapAs({x: 0, value: 2});
+  var series1 = chart.column(mapping1);
+  var series2 = chart.column(mapping2);
+  series1.name("Male");
+  series2.name("Female");
+
+  chart.container("age-distribution-chart");
+  chart.draw();
+});
+
 /* Pie Chart - race distribution by gender 0 */
 anychart.onDocumentReady(function () {
   
   // Create palette.
     var customPalette = anychart.palettes.rangeColors();
-	  customPalette.items(['#9E485C', '#2D0637']);
+    customPalette.items(['#9E485C', '#2D0637']);
     customPalette.count(5);
 
     // create data
@@ -24,8 +82,8 @@ anychart.onDocumentReady(function () {
     chart.title().fontFamily('Outfit');
     chart.title().fontColor('hsla(288, 59%, 22%, 1)');
     chart.title().fontSize(20);
-  chart.background().fill("hsla(0, 88%, 90%, 1)");
-  chart.legend().enabled(false);
+    chart.background().fill("hsla(0, 88%, 90%, 1)");
+    chart.legend().enabled(false);
     chart.legend().itemsLayout("horizontal-expandable");
     chart.legend().fontFamily('Outfit');
     chart.legend().fontSize(10);
@@ -40,7 +98,7 @@ anychart.onDocumentReady(function () {
     chart.draw();
 });
 
-/* Pie Chart - race distribution by gender 0 */
+/* Pie Chart - race distribution by gender 1 */
 anychart.onDocumentReady(function () {
   
   // Create palette.
@@ -83,7 +141,7 @@ anychart.onDocumentReady(function () {
     chart.draw();
 });
 
-
+/* Column chart men rating women */
 anychart.onDocumentReady(function () {
   var chart = anychart.line();
   
@@ -129,6 +187,7 @@ anychart.onDocumentReady(function () {
     chart.draw();
 });
 
+/* Column Chart women rating men on physical attractiveness */
 anychart.onDocumentReady(function () {
   var chart = anychart.line();
   
@@ -136,6 +195,7 @@ anychart.onDocumentReady(function () {
     var customPalette = anychart.palettes.rangeColors();
 	  customPalette.items(['#9E485C', '#2D0637']);
     customPalette.count(1);
+    chart.palette(customPalette);
 
     var lineSeries = chart.column([
         {x: "1.0", value: 0.1},
@@ -169,6 +229,7 @@ anychart.onDocumentReady(function () {
     chart.labels().fontFamily('Outfit');
     chart.labels().fontSize(40);
 
+  
 
     chart.container('rating-vs-decision-chart-women');
     chart.draw();
